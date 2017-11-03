e8381cb91d62ce4ba5ff686f2a3067e9f887f56c
richtier <rikatee@gmail.com>
2017-11-03 16:38:51 +0000

    Fix tests

--- triage/tests/test_views.py
+++ triage/tests/test_views.py
@@ -550,8 +550,10 @@ def test_custom_view_new_exporter(
             casestudies.YORK,
         ]
         assert soup.find('h1').text.replace('\n', '') == (
-            'Use your potential -'
-            'start exporting'
+            'Use your'
+            'potential -'
+            'start'
+            'exporting'
         )
 
 
