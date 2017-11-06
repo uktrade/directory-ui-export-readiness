c4fed9e2d074a9a7464723442cf37cce8ae47b25
richtier <rikatee@gmail.com>
2017-11-06 17:11:06 +0000

    Hide regular exporer question if it was not asked

--- triage/tests/test_templates.py
+++ triage/tests/test_templates.py
@@ -25,3 +25,24 @@ def test_triage_summary_online_marketplace_question(
         assert element.text == expected
     else:
         assert element is None
+
+
+@pytest.mark.parametrize('regular_exporter,expected', (
+    (True, 'Yes'),
+    (False, 'No'),
+    (None, None),
+))
+def test_triage_summary_regular_exporer_question(regular_exporter, expected):
+    context = {
+        'all_cleaned_data': {
+            'regular_exporter': regular_exporter,
+        },
+    }
+    html = render_to_string('triage/wizard-step-summary.html', context)
+    soup = BeautifulSoup(html, 'html.parser')
+    element = soup.find(id='regular_exporter')
+
+    if expected:
+        assert element.text == expected
+    else:
+        assert element is None
