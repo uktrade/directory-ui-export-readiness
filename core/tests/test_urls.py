9eb518d13d903c6c95fc6f13e6a97fb83cbf65f8
Alessandro De Noia <alessandro.denoia@gmail.com>
2017-11-15 14:45:07 +0000

    Add great urls and views

---
+++ core/tests/test_urls.py
@@ -0,0 +1,20 @@
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.parametrize(
+    'view_name,expected_url',
+    (
+        ('landing-page', '/'),
+        ('landing-page-international', '/international'),
+        ('sorry', '/sorry'),
+        ('not-found', '/not-found'),
+        ('finance', '/finance'),
+        ('get-finance-support', '/finance/get-finance-support-from-government'),
+        ('about', '/about'),
+        ('privacy-cookies', '/privacy-cookies'),
+        ('terms-conditions', '/terms-conditions')
+    )
+)
+def test_reverse_urls(view_name, expected_url):
+    assert reverse(view_name) == expected_url
