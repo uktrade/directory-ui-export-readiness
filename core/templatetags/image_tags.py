9a0fe1867c623e7e7b7c77ae1f669f052d05b50b
eadpearce <e.pearce@tutanota.com>
2017-12-19 12:08:12 +0000

    Added ids for content in landing page hero

--- core/templatetags/image_tags.py
+++ core/templatetags/image_tags.py
@@ -5,6 +5,6 @@ register = template.Library()
 
 
 @register.simple_tag
-def progressive_image(webp, src, alt_text=''):
-    context = {'src': src, 'webp': webp, 'alt_text': alt_text}
+def progressive_image(webp, src, img_id='', alt_text=''):
+    context = {'src': src, 'webp': webp, 'alt_text': alt_text, 'img_id': img_id}
     return render_to_string('core/widgets/progressive_image.html', context)
