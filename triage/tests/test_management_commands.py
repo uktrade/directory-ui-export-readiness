10e50ae4d51d8c90bd267646854b8b3b085e88da
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-01-11 11:22:28 +0000

    Fix test

--- triage/tests/test_management_commands.py
+++ triage/tests/test_management_commands.py
@@ -18,7 +18,7 @@ def test_update_top_importers_write_csv(
         mocked_requests,
         mocked_write_csv
 ):
-    with open('triage/tests/comtrade_test.zip', 'r') as test_file:
+    with open('triage/tests/comtrade_test.zip', 'rb') as test_file:
         mocked_requests.get.return_value.iter_content.return_value = \
             stream_file_in_chucks(test_file)
         call_command('update_top_importers')
@@ -27,3 +27,27 @@ def test_update_top_importers_write_csv(
         assert isinstance(
             data, types.GeneratorType
         )
+        expected_data = [
+            {
+                'Partner': 'Antigua and Barbuda',
+                'Partner ISO': 'ATG',
+                'Commodity Code': '01',
+                'Trade Value': '2703'
+            }, {
+                'Partner': 'Azerbaijan',
+                'Partner ISO': 'AZE',
+                'Commodity Code': '01',
+                'Trade Value': '148262'
+            }, {
+                'Partner': 'Argentina',
+                'Partner ISO': 'ARG',
+                'Commodity Code': '93',
+                'Trade Value': '88427'
+            }, {
+                'Partner': 'Australia',
+                'Partner ISO': 'AUS',
+                'Commodity Code': '01',
+                'Trade Value': '25152614'
+            }
+        ]
+        assert list(data) == expected_data
