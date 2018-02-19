9df13fec6b30be0c6db542bbc347d9abc5df1ca4
eadpearce <e.pearce@tutanota.com>
2018-02-19 14:37:56 +0000

    Moved 404 template and updated test

--- core/tests/test_templates.py
+++ core/tests/test_templates.py
@@ -3,8 +3,18 @@ from directory_header_footer.context_processors import urls_processor
 
 
 def test_error_templates(rf):
-    template_name = 'core/404.html'
+    template_name = '404.html'
     assert render_to_string(template_name, {'request': rf.get('/')})
+
+
+def test_404_custom_template(settings, client):
+    settings.DEBUG = False
+    response = client.get('/this-is-not-a-valid-url/')
+    assert response.status_code == 404
+    expected_text = bytes(
+        'If you entered a web address please check'
+        ' it’s correct.', 'utf8')
+    assert expected_text in response.content
 
 
 def test_about_page_services_links(settings):
