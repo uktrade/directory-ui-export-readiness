7068294d78d1a9272885082eff2730af7914d15f
eadpearce <e.pearce@tutanota.com>
2018-02-19 14:50:00 +0000

    Updated not-found url

--- ui/urls.py
+++ ui/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
     ),
     url(
         r"^not-found/$",
-        TemplateView.as_view(template_name='core/404.html'),
+        TemplateView.as_view(template_name='404.html'),
         name='not-found'
     ),
     url(
