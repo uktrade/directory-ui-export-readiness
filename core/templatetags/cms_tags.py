a08710b3a3acac24d389b7ddf0cef2d861ab895d
richtier <rikatee@gmail.com>
2018-04-26 16:15:34 +0100

    Remove standalone title from terms and privacy pages

--- core/templatetags/cms_tags.py
+++ core/templatetags/cms_tags.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 
 from django import template
-from django.utils.text import slugify
+from django.utils.text import slugify, mark_safe
 
 
 register = template.Library()
@@ -22,4 +22,20 @@ def add_anchors(value):
     soup = BeautifulSoup(value, 'html.parser')
     for element in soup.findAll('h2'):
         element.attrs['id'] = build_anchor_id(element)
-    return str(soup)
+    return mark_safe(str(soup))
+
+
+@register.filter
+def add_export_elements_classes(value):
+    soup = BeautifulSoup(value, 'html.parser')
+    mapping = [
+        ('h1', 'heading-xlarge'),
+        ('h2', 'heading-large'),
+        ('h3', 'heading-medium'),
+        ('ul', 'list list-bullet'),
+        ('ol', 'list list-number'),
+    ]
+    for tag_name, class_name in mapping:
+        for element in soup.findAll(tag_name):
+            element.attrs['class'] = class_name
+    return mark_safe(str(soup))
