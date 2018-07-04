581bcf6fab855b3eb7a5723287534712d51ccb77
richtier <rikatee@gmail.com>
2018-07-04 14:31:58 +0100

    Support fallback to local geolocation archive on unable to retrieve maxmind database

--- core/management/commands/download_geolocation_data.py
+++ core/management/commands/download_geolocation_data.py
@@ -1,4 +1,6 @@
-from io import BytesIO
+import abc
+import io
+import logging
 import os
 import tarfile
 
@@ -8,22 +10,22 @@ from django.core.management import BaseCommand
 from django.conf import settings
 
 
-class GeolocationArchive(BytesIO):
+logger = logging.getLogger(__name__)
 
-    @classmethod
-    def from_url(cls, url):
-        response = requests.get(url)
-        response.raise_for_status()
 
-        file_like_object = cls()
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                file_like_object.write(chunk)
-        file_like_object.seek(0)
-        return file_like_object
+class GeolocationArchiveBase(abc.ABC):
+    location = abc.abstractproperty()
+
+    def __init__(self):
+        self.file_like_object = self.retrieve_file()
+
+    @property
+    @abc.abstractmethod
+    def retrieve_file(self):
+        pass
 
     def decompress(self, file_name, destination):
-        tar = tarfile.open(mode="r:gz", fileobj=self)
+        tar = tarfile.open(mode="r:gz", fileobj=self.file_like_object)
         for member in tar.getmembers():
             if member.name.endswith(file_name):
                 member.name = file_name
@@ -33,19 +35,56 @@ class GeolocationArchive(BytesIO):
             raise ValueError(file_name + ' not found in geolocation archive')
 
 
+class GeolocationRemoteFileArchive(GeolocationArchiveBase):
+    location = settings.GEOLOCATION_MAXMIND_DATABASE_FILE_URL
+
+    def retrieve_file(self):
+        file_like_object = io.BytesIO()
+        response = requests.get(self.location)
+        response.raise_for_status()
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                file_like_object.write(chunk)
+        file_like_object.seek(0)
+        return file_like_object
+
+
+class GeolocationLocalFileArchive(GeolocationArchiveBase):
+    location = os.path.join(settings.GEOIP_PATH, 'GeoLite2-Country.tar.gz')
+
+    def retrieve_file(self):
+        file_like_object = io.BytesIO()
+        with open(self.location, 'rb') as f:
+            file_like_object.write(f.read())
+        file_like_object.seek(0)
+        return file_like_object
+
+
+class GeolocationArchiveNegotiator:
+
+    MESSAGE_FAILED_TO_DOWNLOAD = (
+        'Failed to download geolocation archive. Using local archive.'
+    )
+
+    @classmethod
+    def __new__(cls, *args, **kwargs):
+        try:
+            geolocation_archive = GeolocationRemoteFileArchive()
+        except requests.exceptions.HTTPError:
+            logger.error(cls.MESSAGE_FAILED_TO_DOWNLOAD, exc_info=True)
+            geolocation_archive = GeolocationLocalFileArchive()
+        return geolocation_archive
+
+
 class Command(BaseCommand):
 
     help = 'Download the latest geolocation data'
 
     def handle(self, *args, **options):
-
         if not os.path.exists(settings.GEOIP_PATH):
             os.makedirs(settings.GEOIP_PATH)
-
-        compressed_database = GeolocationArchive.from_url(
-            settings.GEOLOCATION_MAXMIND_DATABASE_FILE_URL
-        )
-        compressed_database.decompress(
+        geolocation_archive = GeolocationArchiveNegotiator()
+        geolocation_archive.decompress(
             file_name=settings.GEOIP_COUNTRY,
             destination=settings.GEOIP_PATH
         )
