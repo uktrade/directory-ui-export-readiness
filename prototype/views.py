a8cab9cd61c08dbff7d6bebd2a2c6744e16598d0
eadpearce <e.pearce@tutanota.com>
2018-10-09 14:56:42 +0100

    Add logic to display only fully populated related content items in template

--- prototype/views.py
+++ prototype/views.py
@@ -28,6 +28,31 @@ class ArticleDetailView(
 
     def get_context_data(self, *args, **kwargs):
 
+        has_related_list_item_one = self.page['related_article_one_url'] and \
+            self.page['related_article_one_title']
+        has_related_list_item_two = self.page['related_article_two_url'] and \
+            self.page['related_article_two_title']
+        has_related_list_item_three = self.page['related_article_three_url'] \
+            and self.page['related_article_three_title']
+
+        self.page['has_related_list_item_one'] = has_related_list_item_one
+        self.page['has_related_list_item_two'] = has_related_list_item_two
+        self.page['has_related_list_item_three'] = has_related_list_item_three
+
+        has_related_card_item_one = self.page['related_article_one_url'] \
+            and self.page['related_article_one_title'] and \
+            self.page['related_article_one_teaser']
+        has_related_card_item_two = self.page['related_article_two_url'] \
+            and self.page['related_article_two_title'] and \
+            self.page['related_article_two_teaser']
+        has_related_card_item_three = self.page['related_article_three_url'] \
+            and self.page['related_article_three_title'] and \
+            self.page['related_article_three_teaser']
+
+        self.page['has_related_card_item_one'] = has_related_card_item_one
+        self.page['has_related_card_item_two'] = has_related_card_item_two
+        self.page['has_related_card_item_three'] = has_related_card_item_three
+
         social_links_builder = SocialLinkBuilder(
             self.request.build_absolute_uri(),
             self.page['title'],
