9eb9309e3f6a3d01260d12f3c5e8e1c281b27822
eadpearce <e.pearce@tutanota.com>
2018-10-10 10:47:03 +0100

    Add tests

--- prototype/tests/test_views.py
+++ prototype/tests/test_views.py
@@ -62,7 +62,7 @@ test_topic_page = {
 
 
 @patch('directory_cms_client.client.cms_api_client.lookup_by_full_path')
-def test_prototype_topic_list_page_titles(mock_get_page, client, settings):
+def test_prototype_topic_list_page(mock_get_page, client, settings):
     settings.FEATURE_FLAGS = {
         **settings.FEATURE_FLAGS,
         'PROTOTYPE_PAGES_ON': True,
@@ -89,6 +89,159 @@ def test_prototype_topic_list_page_titles(mock_get_page, client, settings):
     assert 'asia.png' in str(response.content)
     assert 'africa.png' not in str(response.content)
     assert '10 October 2018' in str(response.content)
+
+
+@patch('directory_cms_client.client.cms_api_client.lookup_by_full_path')
+def test_prototype_article_detail_page_no_related_content(
+    mock_get_page, client, settings
+):
+    settings.FEATURE_FLAGS = {
+        **settings.FEATURE_FLAGS,
+        'PROTOTYPE_PAGES_ON': True,
+    }
+
+    test_article_page_no_related_content = {
+        "title": "Test article admin title",
+        "article_title": "Test article",
+        "article_teaser": "Test teaser",
+        "article_image": {"url": "foobar.png"},
+        "article_body_text": "<p>Lorem ipsum</p>",
+        "related_article_one_url": "",
+        "related_article_one_title": "",
+        "related_article_one_teaser": "",
+        "related_article_two_url": "",
+        "related_article_two_title": "",
+        "related_article_two_teaser": "",
+        "related_article_three_url": "",
+        "related_article_three_title": "",
+        "related_article_three_teaser": "",
+        "full_path": "/markets/foo/bar/",
+        "last_published_at": "2018-10-09T16:25:13.142357Z",
+        "meta": {
+            "slug": "bar",
+        },
+    }
+
+    url = reverse('article-detail', kwargs={
+        'topic': 'markets',
+        'list': 'foo',
+        'slug': 'bar',
+    })
+
+    mock_get_page.return_value = create_response(
+        status_code=200,
+        json_body=test_article_page_no_related_content
+    )
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.template_name == ['prototype/article_detail.html']
+
+    assert 'Related content' not in str(response.content)
+
+
+@patch('directory_cms_client.client.cms_api_client.lookup_by_full_path')
+def test_prototype_article_detail_page_one_related_content(
+    mock_get_page, client, settings
+):
+    settings.FEATURE_FLAGS = {
+        **settings.FEATURE_FLAGS,
+        'PROTOTYPE_PAGES_ON': True,
+    }
+
+    article_page = {
+        "title": "Test article admin title",
+        "article_title": "Test article",
+        "article_teaser": "Test teaser",
+        "article_image": {"url": "foobar.png"},
+        "article_body_text": "<p>Lorem ipsum</p>",
+        "related_article_one_url": "#",
+        "related_article_one_title": "Foo",
+        "related_article_one_teaser": "Bar",
+        "related_article_two_url": "",
+        "related_article_two_title": "",
+        "related_article_two_teaser": "",
+        "related_article_three_url": "",
+        "related_article_three_title": "",
+        "related_article_three_teaser": "",
+        "full_path": "/markets/foo/bar/",
+        "last_published_at": "2018-10-09T16:25:13.142357Z",
+        "meta": {
+            "slug": "bar",
+        },
+    }
+
+    url = reverse('article-detail', kwargs={
+        'topic': 'markets',
+        'list': 'foo',
+        'slug': 'bar',
+    })
+
+    mock_get_page.return_value = create_response(
+        status_code=200,
+        json_body=article_page
+    )
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.template_name == ['prototype/article_detail.html']
+
+    assert 'Related content' in str(response.content)
+    assert '<a href="#" class="link">Foo</a>' in str(response.content)
+
+
+@patch('directory_cms_client.client.cms_api_client.lookup_by_full_path')
+def test_prototype_article_detail_page_two_related_content(
+    mock_get_page, client, settings
+):
+    settings.FEATURE_FLAGS = {
+        **settings.FEATURE_FLAGS,
+        'PROTOTYPE_PAGES_ON': True,
+    }
+
+    article_page = {
+        "title": "Test article admin title",
+        "article_title": "Test article",
+        "article_teaser": "Test teaser",
+        "article_image": {"url": "foobar.png"},
+        "article_body_text": "<p>Lorem ipsum</p>",
+        "related_article_one_url": "",
+        "related_article_one_title": "",
+        "related_article_one_teaser": "",
+        "related_article_two_url": "#2",
+        "related_article_two_title": "Foo 2",
+        "related_article_two_teaser": "Bar 2",
+        "related_article_three_url": "#3",
+        "related_article_three_title": "Foo 3",
+        "related_article_three_teaser": "Bar 3",
+        "full_path": "/markets/foo/bar/",
+        "last_published_at": "2018-10-09T16:25:13.142357Z",
+        "meta": {
+            "slug": "bar",
+        },
+    }
+
+    url = reverse('article-detail', kwargs={
+        'topic': 'markets',
+        'list': 'foo',
+        'slug': 'bar',
+    })
+
+    mock_get_page.return_value = create_response(
+        status_code=200,
+        json_body=article_page
+    )
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.template_name == ['prototype/article_detail.html']
+
+    assert 'Related content' in str(response.content)
+    assert '<a href="#2" class="link">Foo 2</a>' in str(response.content)
+    assert '<a href="#3" class="link">Foo 3</a>' in str(response.content)
 
 
 @patch('directory_cms_client.client.cms_api_client.lookup_by_full_path')
