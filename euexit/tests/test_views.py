075ce5cd78090fdbb95c6761be6e805c709231f8
richtier <rikatee@gmail.com>
2018-10-11 13:32:09 +0100

    Include ingress and form url in zendesk submission

--- euexit/tests/test_views.py
+++ euexit/tests/test_views.py
@@ -1,4 +1,5 @@
 from unittest import mock
+from urllib.parse import urljoin
 
 from directory_constants.constants import choices
 import pytest
@@ -86,7 +87,7 @@ def test_international_form_cms_retrieval_ok(
 
 @mock.patch('directory_cms_client.client.cms_api_client.lookup_by_slug')
 @mock.patch.object(
-    views.InternationalContactFormView.form_class, 'save'
+    views.InternationalContactFormView.form_class.action_class, 'save'
 )
 def test_international_form_submit(
     mock_save, mock_lookup_by_slug, settings, client, captcha_stub
@@ -100,6 +101,9 @@ def test_international_form_submit(
     }
 
     url = reverse('eu-exit-international-contact-form')
+
+    # sets referrer in the session
+    client.get(url, {}, HTTP_REFERER='http://www.google.com')
     response = client.post(url, {
         'first_name': 'test',
         'last_name': 'example',
@@ -119,11 +123,18 @@ def test_international_form_submit(
     )
 
     assert mock_save.call_count == 1
-    assert mock_save.call_args == mock.call(
-        email_address='test@example.com',
-        full_name='test example',
-        subject='EU Exit international contact form'
-    )
+    assert mock_save.call_args == mock.call({
+        'first_name': 'test',
+        'last_name': 'example',
+        'email': 'test@example.com',
+        'organisation_type': 'COMPANY',
+        'company_name': 'thing',
+        'country': 'AL',
+        'city': 'London',
+        'comment': 'hello',
+        'form_url': 'http://testserver/eu-exit/international/contact/',
+        'ingress_url': 'http://www.google.com',
+    })
 
 
 @pytest.mark.parametrize('url,template_name', [
@@ -231,7 +242,7 @@ def test_domestic_form_cms_retrieval_ok(
 
 @mock.patch('directory_cms_client.client.cms_api_client.lookup_by_slug')
 @mock.patch.object(
-    views.DomesticContactFormView.form_class, 'save'
+    views.DomesticContactFormView.form_class.action_class, 'save'
 )
 def test_domestic_form_submit(
     mock_save, mock_lookup_by_slug, settings, client, captcha_stub
@@ -245,6 +256,9 @@ def test_domestic_form_submit(
     }
 
     url = reverse('eu-exit-domestic-contact-form')
+
+    # sets referrer in the session
+    client.get(url, {}, HTTP_REFERER='http://www.google.com')
     response = client.post(url, {
         'first_name': 'test',
         'last_name': 'example',
@@ -260,10 +274,58 @@ def test_domestic_form_submit(
     assert response.url == reverse(
         'eu-exit-domestic-contact-form-success'
     )
-
     assert mock_save.call_count == 1
-    assert mock_save.call_args == mock.call(
-        email_address='test@example.com',
-        full_name='test example',
-        subject='EU Exit international contact form'
+    assert mock_save.call_args == mock.call({
+        'first_name': 'test',
+        'last_name': 'example',
+        'email': 'test@example.com',
+        'organisation_type': 'COMPANY',
+        'company_name': 'thing',
+        'comment': 'hello',
+        'form_url': 'http://testserver/eu-exit/contact/',
+        'ingress_url': 'http://www.google.com',
+    })
+
+
+@pytest.mark.parametrize('url', (
+    reverse('eu-exit-international-contact-form'),
+    reverse('eu-exit-domestic-contact-form'),
+))
+@mock.patch('directory_cms_client.client.cms_api_client.lookup_by_slug')
+def test_form_urls(mock_lookup_by_slug, settings, client, url):
+    mock_lookup_by_slug.return_value = create_response(
+        status_code=200, json_body={}
     )
+    settings.FEATURE_FLAGS = {
+        **settings.FEATURE_FLAGS,
+        'HIGH_POTENTIAL_OPPORTUNITIES_ON': True
+    }
+
+    response = client.get(url, {}, HTTP_REFERER='http://www.google.com')
+
+    assert response.status_code == 200
+    form = response.context_data['form']
+    assert form.form_url == urljoin('http://testserver', url)
+    assert form.ingress_url == 'http://www.google.com'
+
+
+@pytest.mark.parametrize('url', (
+    reverse('eu-exit-international-contact-form'),
+    reverse('eu-exit-domestic-contact-form'),
+))
+@mock.patch('directory_cms_client.client.cms_api_client.lookup_by_slug')
+def test_form_urls_no_referer(mock_lookup_by_slug, settings, client, url):
+    mock_lookup_by_slug.return_value = create_response(
+        status_code=200, json_body={}
+    )
+    settings.FEATURE_FLAGS = {
+        **settings.FEATURE_FLAGS,
+        'HIGH_POTENTIAL_OPPORTUNITIES_ON': True
+    }
+
+    response = client.get(url, {})
+
+    assert response.status_code == 200
+    form = response.context_data['form']
+    assert form.form_url == urljoin('http://testserver', url)
+    assert form.ingress_url is None
